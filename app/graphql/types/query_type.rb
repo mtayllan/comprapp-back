0d8c422a3c12e38ee4d1b9d941efec17fb33a207
mtayllan <messiastayllan@gmail.com>
2020-01-15 10:49:55 -0300

    Register main query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,13 +8,29 @@ module Types
           [Types::ProductType],
           null: false,
           description: 'Returns a list of products'
+    field :orders,
+          [Types::OrderType],
+          null: false,
+          description: 'Returns a list of orders'
 
     def users
-      User.all
+      return User.all if context[:current_user]&.has_role?(:admin)
+
+      []
     end
 
     def products
       Product.all
+    end
+
+    def orders
+      current_user = context[:current_user]
+      if current_user&.has_role?(:admin)
+        Order.all
+      elsif current_user&.has_role?(:customer)
+        current_user.orders
+      end
+      []
     end
   end
 end
