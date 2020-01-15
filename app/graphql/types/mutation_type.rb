f382cace1f794b8f887144382c6987bd00336f5a
mtayllan <messiastayllan@gmail.com>
2020-01-15 03:01:03 -0300

    correct mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,13 +1,13 @@
 module Types
   class MutationType < Types::BaseObject
     # user
-    field :create_user, mutation: Mutations::CreateUserMutation
-    field :update_user, mutation: Mutations::UpdateUserMutation
-    field :delete_user, mutation: Mutations::DeleteUserMutation
+    field :createUser, mutation: Mutations::CreateUserMutation
+    field :updateUser, mutation: Mutations::UpdateUserMutation
+    field :deleteUser, mutation: Mutations::DeleteUserMutation
 
     # address
-    field :create_address, mutation: Mutations::CreateAddressMutation
-    field :update_address, mutation: Mutations::UpdateAddressMutation
-    field :delete_address, mutation: Mutations::DeleteAddressMutation
+    field :createAddress, mutation: Mutations::CreateAddressMutation
+    field :updateAddress, mutation: Mutations::UpdateAddressMutation
+    field :deleteAddress, mutation: Mutations::DeleteAddressMutation
   end
 end
