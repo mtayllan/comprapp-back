ac3e686d1c40c914fc96c25dfae2bc6f6a14fb19
mtayllan <messiastayllan@gmail.com>
2020-01-15 02:21:50 -0300

    Register Mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,10 @@ module Types
     field :create_user, mutation: Mutations::CreateUserMutation
     field :update_user, mutation: Mutations::UpdateUserMutation
     field :delete_user, mutation: Mutations::DeleteUserMutation
+
+    # address
+    field :create_address, mutation: Mutations::CreateAddressMutation
+    field :update_address, mutation: Mutations::UpdateAddressMutation
+    field :delete_address, mutation: Mutations::DeleteAddressMutation
   end
 end
