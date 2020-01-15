79bb8eec5656baeb74ebcf80390f691e34e2979f
mtayllan <messiastayllan@gmail.com>
2020-01-15 04:11:29 -0300

    Register unity and product mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -9,5 +9,15 @@ module Types
     field :createAddress, mutation: Mutations::CreateAddressMutation
     field :updateAddress, mutation: Mutations::UpdateAddressMutation
     field :deleteAddress, mutation: Mutations::DeleteAddressMutation
+
+    # unity
+    field :createUnity, mutation: Mutations::CreateUnityMutation
+    field :updateUnity, mutation: Mutations::UpdateUnityMutation
+    field :deleteUnity, mutation: Mutations::DeleteUnityMutation
+
+    # product
+    field :createProduct, mutation: Mutations::CreateProductMutation
+    field :updateProduct, mutation: Mutations::UpdateProductMutation
+    field :deleteProduct, mutation: Mutations::DeleteProductMutation
   end
 end
